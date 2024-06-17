f0ecc1753ac06fcc6b602606383bc4d4b5a1182e
Brenda Ondimu <brendaondimu@gmail.com>
2024-06-17 23:08:56 +0300

    new changes

--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Product 
 
-# Create your views here.
+
+# Create your views here
+def product_list(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request,'htmlfile'.context)
